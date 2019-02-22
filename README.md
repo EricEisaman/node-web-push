4345c4c8da41c77aa37dec0cd059de013c55f7e7
Glitch (node-web-push) <none>
2019-02-22 20:08:33 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# node-web-push
+# web-push-demo
+"Hello, World" with web push in Node.js
+
+## Remix then:
+
+Generate VAPID keys in **Console**
+```
+./node_modules/.bin/web-push generate-vapid-keys
+```
+Edit **.env**
+```
+PUBLIC_VAPID_KEY=YourPublicVAPIDKey
+PRIVATE_VAPID_KEY=YourPrivateVAPIDKey
+```
+
+Edit **public/client.js
+```
+const publicVapidKey = YOUR PUBLIC VAPID KEY
+```
+
+<a href="https://thecodebarbarian.com/sending-web-push-notifications-from-node-js.html" target="_blank" rel="noreferrer">Article</a>
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const webpush = require('web-push');
+
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+
+// Replace with your email
+webpush.setVapidDetails('mailto:no-reply@cs1.org', publicVapidKey, privateVapidKey);
+
+const app = express();
+
+app.use(require('body-parser').json());
+
+app.post('/subscribe', (req, res) => {
+  const subscription = req.body;
+  res.status(201).json({});
+  const payload = JSON.stringify({ title: 'test' });
+
+  console.log(subscription);
+
+  webpush.sendNotification(subscription, payload).catch(error => {
+    console.error(error.stack);
+    console.log(error.body);
+  });
+});
+
+app.use(require('express-static')('./public'));
+
+app.listen(3000);
